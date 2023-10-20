742b12e0d7206237ae0dfb2f2c420230b1e988f0
Mikolaj <mikolaj.zielichowski@neos.co.uk>
2023-10-20 07:54:07 +0200

    Initial Commit

---
+++ Leftovers/LeftoversApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftoversApp.swift
+//  Leftovers
+//
+//  Created by Mikolaj Zelichowski on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeftoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
